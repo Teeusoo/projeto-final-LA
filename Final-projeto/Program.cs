49a08cb23b79afdb025d5b4355fc906e873da19e
Teeusoo <matheus02@msn.com>
2017-11-29 11:51:24 -0200

    Tratando erros com excecao personalizada

--- Final-projeto/Program.cs
+++ Final-projeto/Program.cs
@@ -6,15 +6,18 @@ namespace Final_projeto {
     class Program {
         static void Main(string[] args) {
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new torre(tab, cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new torre(tab, cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new rei(tab, cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new torre(tab, cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new torre(tab, cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new rei(tab, cor.Preta), new Posicao(0, 2));
 
-            tela.imprimirTabuleiro(tab);
-
-
+                tela.imprimirTabuleiro(tab);
+            }
+            catch (tabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
 
         }
---
+++ Final-projeto/tabuleiro/tabuleiroException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace tabuleiro {
+    class tabuleiroException : Exception { 
+
+        public tabuleiroException(string msg) : base(msg) {
+
+        }
+    }
+}
